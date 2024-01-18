808fecdee7ec51a55d3468952ea7afaf4d37f5cd
Hamza Liaqat <130157273+hmzahliaqat@users.noreply.github.com>
2024-01-18 10:31:13 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/tw-elements/dist/js/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, typography,require("tw-elements/dist/plugin.cjs")],
+};
